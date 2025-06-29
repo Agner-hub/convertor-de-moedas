bc49279ddbae8e30d809d3156ccacfaa4448079f
Agner-hub <agnerchristian884@gmail.com>
2025-06-29 20:57:32 -0300

    conversor de moedas funcionando e substituindo

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
 function botao(){
-    alert("converteu")
+    let paraConverter = document.querySelector(".to-Convert");
+    let convertida = document.querySelector(".convert");
+    let inputValue = document.querySelector(".input-value").value;
+    
+    const dollar = 5.2;
+
+    const conversao = new Intl.NumberFormat("en-US",/*linguagem/País */ {
+        style: "currency",/*Tipo,no caso, moeda */
+        currency: "USD",/*Sigla dp país */
+    }).format(inputValue / dollar);/*O que formatar */
+
+    paraConverter.innerHTML = new Intl.NumberFormat("pt-Br", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputValue);
+    
+    convertida.innerHTML = (conversao);
+    
+
+    
 }
